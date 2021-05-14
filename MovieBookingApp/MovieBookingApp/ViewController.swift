38e5a841943f7478b81a1224b17fe3542179e80e
zawhtetnaing10 <zawgyi.gog@gmail.com>
2021-05-15 00:31:23 +0630

    Movie booking app side menu and skeleton for movie time screen implemented

---
+++ MovieBookingApp/MovieBookingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBookingApp
+//
+//  Created by Zaw Htet Naing on 07/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
